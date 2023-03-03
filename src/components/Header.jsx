2e8519e2bf8cd59e8b79cb3820b585e53305bfd9
Saurabh Gupta <saurabh.gupta@inmorphis.com>
2023-03-03 19:21:42 +0530

    Header-Navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+import {images} from '../constants'
+import {AiOutlineMenu, AiOutlineClose, } from 'react-icons/ai'
+import {MdKeyboardArrowDown} from 'react-icons/md'
+
+const navItemsInfo = [
+    {name: "Home" , type: "link" },
+    {name: "Articles" , type: "link" },
+    {name: "Pages" , type: "dropdown", items: ["About us", "Contact us"] },
+    {name: "Pricing" , type: "link" },
+    {name: "Faq" , type: "link" }
+]
+
+const NavItem = ({item}) => {
+    return ( <li className='relative group'>
+        {item.type === "link" ? ( <><a href="/" className='px-4 py-2'>
+            {item.name}
+            </a>
+    <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>/</span></>
+     ) : (
+            <>
+            <a href="/" className='px-4 py-2 flex gap-x-1 items-center'>
+                <span>{item.name}</span>
+                <MdKeyboardArrowDown />
+            </a>
+            <div className='hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max'>
+            <ul className='flex flex-col shadow-lg rounded-lg overflow-hidden'>
+            {item.items.map((page) => (
+                    <a href="/" className='hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft'>
+                        {page}
+                    </a>
+            ))}
+        </ul>
+        </div></>) }
+    
+</li>
+    )
+}
+const Header = () => {
+
+    const [navIsVisible, setNavIsVisible] = useState(false);
+
+    const navVisibility = () => {
+        setNavIsVisible((curState) => {
+            return !curState;
+        })
+    }
+
+  return (
+    <section>
+        <header className="container mx-zuto px-5 flex justify-between py-4 items-center">
+            <div>
+                <img className='w-16' src={images.Logo} alt="logo" />
+            </div>
+            <div className='z-50 lg:hidden'>
+                {navIsVisible ? <AiOutlineClose className = "w-6 h-6" onClick={navVisibility} /> : <AiOutlineMenu className = "w-6 h-6" onClick={navVisibility} /> }
+            </div>
+            <div className={`${navIsVisible ? "right-0" : "-right-full"} transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static  flex gap-x-9 items-center`}>
+                <ul className='text-white items-center lg:text-dark-soft flex flex-col lg:flex-row py-10 gap-y-5 gap-x-3 font-semibold'>
+                    {navItemsInfo.map((item) => {
+                        return <NavItem key={item.name} item={item}/>
+                    })}
+                </ul>
+                <button className='mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign In</button>
+            </div>
+        </header>
+    </section>
+  )
+}
+
+export default Header
