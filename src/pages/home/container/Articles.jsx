e5e17c5f2e474fbe0f73c4b9e49687f70872fd9c
Saurabh Gupta <saurabh.gupta@inmorphis.com>
2023-03-06 14:34:16 +0530

    CTA Section

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ArticlesCard from '../../../components/ArticlesCard'
+import {FaArrowRight} from 'react-icons/fa'
 
 const Articles = () => {
   return (
-    <section className='container mx-auto flex flex-wrap md:gap-x-5 gap-y-5 px-5 py-10'>
+    <section className='container flex flex-col mx-auto px-5 py-10'>
+        <div className='flex flex-wrap md:gap-x-5 gap-y-5 pb-10'>
         <ArticlesCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
         <ArticlesCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
         <ArticlesCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
+        </div>
+        <button className='mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg'>
+            <span>More Articles</span>
+            <FaArrowRight className='w-3 h-3'/>
+        </button>
     </section>
   )
 }
