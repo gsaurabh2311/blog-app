a971e402b39487066f4f997e8b84c49645d70f8c
Saurabh Gupta <saurabh.gupta@inmorphis.com>
2023-03-06 12:26:27 +0530

    ArticleCard

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,10 @@
 import Logo from '../assets/Logo.svg'
 import HeroImage from '../assets/HeroImage.svg'
+import PostImage from '../assets/posts/post1.jpg'
+import PostProfile from '../assets/posts/post-profile.svg'
 
 const images = {
-    Logo,HeroImage,
+    Logo,HeroImage,PostImage,PostProfile
 }
 
 export default images
